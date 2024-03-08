c64eb86aa3d23ca94ffce2f9537b097ecf465c9d
ForAeons <w.xy020203@gmail.com>
2024-03-08 10:46:32 +0800

    Restructure code into cargo workspaces

--- src/prelude.rs
+++
@@ -1,8 +0,0 @@
-//! Crate prelude
-
-pub use crate::error::Error;
-
-pub type Result<T> = core::result::Result<T, Error>;
-
-// Generic Wrapper tuple struct for newtype pattern
-pub struct W<T>(pub T);

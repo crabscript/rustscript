a97380c53e61ae2402689e21334639719f38da90
ForAeons <w.xy020203@gmail.com>
2024-03-08 15:20:51 +0800

    Add token types

--- src/lexer/src/lib.rs
+++ src/lexer/src/lib.rs
@@ -1,14 +1,24 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+#![allow(unused)]
+
+use token::Token;
+use token::TokenKind;
+
+mod token;
+
+pub fn tokenize(s: &mut str) -> Vec<Token> {
+    let mut tokens = Vec::new();
+
+    loop {
+        let next_token = next(s);
+        match next_token.kind {
+            TokenKind::Eof => break,
+            _ => tokens.push(next_token),
+        }
+    }
+
+    tokens
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+fn next(s: &mut str) -> Token {
+    todo!()
 }

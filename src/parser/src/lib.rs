ce25f5216df9a2156daf1291887ba4808a9740f7
Huzaifa Raghav <huzaifaraghav2000@gmail.com>
2024-03-10 23:03:42 +0800

    Add basic parse types

--- src/parser/src/lib.rs
+++ src/parser/src/lib.rs
@@ -2,13 +2,30 @@ pub fn add(left: usize, right: usize) -> usize {
     left + right
 }
 
+enum Expr {
+    Integer(i64)
+}
+
+enum Decl {
+    ExprStmt(Expr)
+}
+
+// Last expression is value of program semantics (else Unit type)
+// Program is either one declaration or a sequence of declarations with optional last expression
+    // Decl is there to avoid needing to do recursive Seq for a bunch of stmts in order
+enum ASTNode {
+    Decl,
+    Seq(Vec<ASTNode>, Option<Expr>)
+}
+
+
 #[cfg(test)]
 mod tests {
-    use super::*;
-
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    fn can_lex() {
+        let m = String::from("let x = 2;");
+        let res = lexer::lex(m.as_str());
+        let res = res.collect::<Vec<_>>();
+        dbg!(res);
     }
 }

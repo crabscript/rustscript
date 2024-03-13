365a6a485f141a09d89258b4ecd4219d7092aae3
Huzaifa Raghav <huzaifaraghav2000@gmail.com>
2024-03-13 18:01:22 +0800

    Add template for modular rewrite

--- src/parser/src/lib.rs
+++ src/parser/src/lib.rs
@@ -1,4 +1,6 @@
+use std::any::Any;
 use std::fmt::Display;
+use std::iter::Peekable;
 
 use lexer::Token;
 use logos::Lexer;
@@ -81,57 +83,119 @@ impl Display for ParseError {
 }
 
 pub struct Parser<'inp> {
-    lexer: Lexer<'inp, Token>,
+    decls: Vec<Decl>,
+    prev_tok:Option<Token>,
+    lexer: Peekable<Lexer<'inp, Token>>,
 }
 
 impl<'inp> Parser<'inp> {
     pub fn new<'src>(lexer: Lexer<'src, Token>) -> Parser<'src> {
-        Parser { lexer }
+        Parser { decls:vec![], prev_tok:None, lexer: lexer.peekable() }
     }
 
     // Assume input is valid: no double semi or double expr. semicolon means prev_tok has a value
-    pub fn parse(mut self) -> Result<Program, ParseError> {
-        let mut decls: Vec<Decl> = vec![];
-        let mut prev_tok: Option<Token> = None;
+    // pub fn parse(mut self) -> Result<Program, ParseError> {
+    //     let mut decls: Vec<Decl> = vec![];
+    //     let mut prev_tok: Option<Token> = None;
 
-        for tok_res in self.lexer.into_iter() {
-            let tok = tok_res.expect("Expect token");
+    //     for tok_res in self.lexer.into_iter() {
+    //         let tok = tok_res.expect("Expect token");
     
-            match tok {
-                Token::Integer(_) => {
-                    if prev_tok.is_some() {
-                        return Err(ParseError::new("Consecutive expressions not allowed"));
-                    }
-                    prev_tok.replace(tok);
-                }
-                Token::Semi => {
-                    let tok = prev_tok
-                        .clone()
-                        .expect("Expected expression before semicolon");
-                    match tok {
-                        Token::Integer(val) => decls.push(Decl::ExprStmt(Expr::Integer(val))),
-                        _ => return Err(ParseError::new("Unexpected token type")),
-                    }
-                    prev_tok.take();
-                }
-                _ => unimplemented!()
+    //         match tok {
+    //             Token::Integer(_) => {
+    //                 if prev_tok.is_some() {
+    //                     return Err(ParseError::new("Consecutive expressions not allowed"));
+    //                 }
+    //                 prev_tok.replace(tok);
+    //             }
+    //             Token::Semi => {
+    //                 let tok = prev_tok
+    //                     .clone()
+    //                     .expect("Expected expression before semicolon");
+    //                 match tok {
+    //                     Token::Integer(val) => decls.push(Decl::ExprStmt(Expr::Integer(val))),
+    //                     _ => return Err(ParseError::new("Unexpected token type")),
+    //                 }
+    //                 prev_tok.take();
+    //             }
+    //             _ => unimplemented!()
+    //         }
+    //     }
+
+    //     let mut ret_expr: Option<Expr> = None;
+    //     if let Some(tok) = prev_tok {
+    //         match tok {
+    //             Token::Integer(val) => {
+    //                 ret_expr.replace(Expr::Integer(val));
+    //             }
+    //             _ => return Err(ParseError::new("Unexpected token type")),
+    //         }
+    //     }
+
+    //     Ok(Program {
+    //         decls,
+    //         last_expr: ret_expr,
+    //     })
+    // }
+
+    // expect peek to be semicolon
+    fn expect_semicolon(&mut self)->Result<(), ParseError> {
+        let err = Err(ParseError::new("Expected semicolon")); 
+        let pk = self.lexer.peek();
+
+        if pk.is_none() {
+            err
+        } else {
+            let pk = pk.expect("Expect lexer to succeed").as_ref().expect("Peek has something");
+            match pk {
+                Token::Semi => Ok(()),
+                _ => err
             }
         }
+    }
 
-        let mut ret_expr: Option<Expr> = None;
-        if let Some(tok) = prev_tok {
-            match tok {
-                Token::Integer(val) => {
-                    ret_expr.replace(Expr::Integer(val));
-                }
-                _ => return Err(ParseError::new("Unexpected token type")),
-            }
+    fn advance(&mut self) {
+        if let Some(val) = self.lexer.peek() {
+            self.prev_tok.replace(val.clone().expect("Expect lexer to succeed"));
+            self.lexer.next();
+        }
+    }
+
+    fn parse_atomic(&mut self) {
+        dbg!("atomic:", &self.prev_tok);
+    }   
+
+    fn parse_decl(&mut self) {
+        dbg!(&self.prev_tok);
+        dbg!(&self.lexer.peek());
+
+        let prev_tok = self.prev_tok.as_ref().expect("prev_tok should not be empty");
+        match prev_tok {
+            Token::Integer(_) | Token::Float(_) | Token::Bool(_) => {
+                self.parse_atomic()
+            },
+            _ => unimplemented!()
+        }
+        self.advance(); // should be called once done (consume semicolon)
+    }
+
+    pub fn parse(mut self)->Result<Program,ParseError> {
+        self.advance();
+
+        while let Some(_) = self.lexer.peek() {
+            self.parse_decl();
+            // need to call lexer.next() at least once somewhere so the loop breaks
+            self.advance();
+            // break;
         }
 
-        Ok(Program {
-            decls,
-            last_expr: ret_expr,
-        })
+        let last_expr:Option<Expr> = None;
+        Ok(
+            Program {
+                decls: self.decls,
+                last_expr
+            }
+        )
     }
 }
 
@@ -157,7 +221,13 @@ mod tests {
         } else {
             assert_eq!(res.to_string(), exp_err);
         }
-        
+    }
+
+    #[test]
+    fn play() {
+        let mut lexer = Token::lexer("20;");
+        let p = Parser::new(lexer);
+        p.parse();
     }
 
     #[test]
@@ -167,8 +237,13 @@ mod tests {
         test_parse(" 20 ;30 \n ", "20;30"); // exprstmt, expr
         test_parse(" 20 ;30; \n ", "20;30;"); // exprstmt, exprsmt
         test_parse(" 20 ;30; \n40 \n ", "20;30;40"); // two exprstmt + expr
-
     }
+
+    // #[test]
+    // fn test_parse_floats() {
+    //     test_parse(" 2.2\n ", "2.2"); // expr only
+    //     test_parse(" 2.23\n ", "2.23;"); // expr only
+    // }
 
     #[test]
     fn test_errs_for_consecutive_exprs() {

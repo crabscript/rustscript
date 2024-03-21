5f34793a5d3a6b9d5ab43ef6e723e2c778f5d8ec
Huzaifa Raghav <huzaifaraghav2000@gmail.com>
2024-03-21 13:24:02 +0800

    Add repl

--- vm/ignite/src/repl.rs
+++ vm/ignite/src/repl.rs
@@ -1,6 +1,36 @@
 use anyhow::Result;
 use compiler::compile_string;
+use rustyline::{DefaultEditor, error::ReadlineError};
 
 pub fn ignite_repl() -> Result<()>  {
+    let mut rl = DefaultEditor::new().unwrap();
+    println!("Welcome to the RustScript REPL! Type /exit to exit.");
+    println!();
+
+    loop {
+        let readline = rl.readline(">>> ");
+
+        match readline {
+            Ok(inp) => {
+                let inp = inp.trim().to_string();
+
+                if inp.len() == 0 {
+                    continue;
+                }
+
+                if inp.eq("/exit") {
+                    println!("See you again!");
+                    break;
+                }
+
+                rl.add_history_entry(inp.clone().trim()).unwrap();
+
+                let compiled = compile_string(&inp)?;
+                
+            },
+            _ => ()
+        }
+    }
+
     Ok(())
 }

25e46263ca95c4f9b051e7ee1f549e53f5ca8610
Huzaifa Raghav <huzaifaraghav2000@gmail.com>
2024-03-27 11:41:03 +0800

    Add type check for symbol and type env

--- src/types/src/type_checker.rs
+++ src/types/src/type_checker.rs
@@ -1,5 +1,5 @@
 use parser::Type;
-use std::fmt::Display;
+use std::{collections::HashMap, fmt::Display};
 
 use parser::{BlockSeq, Decl, Expr};
 
@@ -58,12 +58,21 @@ impl<'prog> TypeChecker<'prog> {
 
     /// Return the type errors out instead of using mutable ref
     // because for nested errors in the expr we want to propagate those
-    fn check_expr(expr: &Expr) -> Result<Type, TypeErrors> {
+    fn check_expr(expr: &Expr, ty_env: &mut HashMap<String, Type>) -> Result<Type, TypeErrors> {
         let local_errs = TypeErrors::new();
         let ty = match expr {
             Expr::Integer(_) => Type::Int,
             Expr::Float(_) => Type::Float,
             Expr::Bool(_) => Type::Bool,
+            Expr::Symbol(ident) => {
+                let get_type = ty_env.get(ident);
+                if get_type.is_none() {
+                    let e = format!("Identifier '{}' not declared", ident);
+                    return Err(TypeErrors::new_err(&e));
+                }
+
+                get_type.expect("Should have type").to_owned()
+            }
             _ => todo!(),
         };
 
@@ -75,10 +84,11 @@ impl<'prog> TypeChecker<'prog> {
     }
 
     /// Type check declaration and add errors if any
-    fn check_decl(decl: &Decl) -> Result<(), TypeErrors> {
+    fn check_decl(decl: &Decl, ty_env: &mut HashMap<String, Type>) -> Result<(), TypeErrors> {
+        dbg!("Type checking decl:", decl);
         match decl {
             Decl::LetStmt(stmt) => {
-                let expr_type = TypeChecker::check_expr(&stmt.expr)?;
+                let expr_type = TypeChecker::check_expr(&stmt.expr, ty_env)?;
                 if let Some(ty_ann) = stmt.type_ann {
                     // annotation and assigned type not equal
                     if !ty_ann.eq(&expr_type) {
@@ -90,7 +100,12 @@ impl<'prog> TypeChecker<'prog> {
                     }
                 }
 
-                // assign type to binding
+                // assign type to ident
+                ty_env.insert(stmt.ident.to_owned(), expr_type);
+            }
+            // Type check the expr and return any errors
+            Decl::ExprStmt(expr) => {
+                TypeChecker::check_expr(expr, ty_env)?;
             }
             _ => todo!(),
         }
@@ -100,16 +115,18 @@ impl<'prog> TypeChecker<'prog> {
 
     pub fn type_check(self) -> Result<Type, TypeErrors> {
         let mut errs = TypeErrors::new();
+        // map bindings to types
+        let mut ty_env: HashMap<String, Type> = HashMap::new();
 
         for decl in self.program.decls.iter() {
-            if let Err(mut decl_errs) = TypeChecker::check_decl(decl) {
+            if let Err(mut decl_errs) = TypeChecker::check_decl(decl, &mut ty_env) {
                 errs.append(&mut decl_errs)
             }
         }
 
         // Return type of last expr if any. If errs, add to err list
         if let Some(last) = &self.program.last_expr {
-            let res = TypeChecker::check_expr(last);
+            let res = TypeChecker::check_expr(last, &mut ty_env);
             match res {
                 Ok(ty) => return Ok(ty),
                 Err(mut expr_errs) => errs.append(&mut expr_errs),
@@ -189,6 +206,14 @@ mod tests {
         // Multiple errors: collects them
         expect_err("let x : float = 20; let x : int = true; let x : float = 20;",
          "[TypeError]: 'x' has declared type float but assigned type int\n[TypeError]: 'x' has declared type int but assigned type bool\n[TypeError]: 'x' has declared type float but assigned type int", false);
+    }
+
+    #[test]
+    fn test_type_check_sym() {
+        expect_pass("let x : int = 2; x", Type::Int);
+        // // variable shadowing
+        expect_pass("let x : int = 2; let x : bool = true; x", Type::Bool);
+        expect_pass("let x : int = 2; let y : bool = true; x;", Type::Unit);
     }
 
     #[test]

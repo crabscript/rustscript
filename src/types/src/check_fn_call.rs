b31b89b2a8818efcf7205c059eba4e617d7d3cef
Huzaifa Raghav <huzaifaraghav2000@gmail.com>
2024-04-17 02:56:04 +0800

    Add type checking for builtins

--- src/types/src/check_fn_call.rs
+++ src/types/src/check_fn_call.rs
@@ -1,9 +1,7 @@
 use crate::type_checker::{CheckResult, TypeChecker, TypeErrors};
 use parser::structs::{FnCallData, Type};
 
-// const BUILTINS: [&str; 19] = ["read_line", "print", "println", "string_len", "min", "max", "abs", "cos",
-// "sin", "tan", "sqrt", "log", "pow", "itoa", "atoi", "float_to_int", "int_to_float", "sem_create", "sem_set"];
-
+// Ideally these constants should be shared across type checker and VM but I don't want to waste time refactoring
 const READ_LINE: &str = "read_line";
 const PRINT: &str = "print";
 const PRINTLN: &str = "println";
@@ -52,74 +50,286 @@ impl<'prog> TypeChecker<'prog> {
         BUILTINS.contains(&name)
     }
 
+    fn get_type_string(arg_types: &[Type]) -> String {
+        let r = arg_types
+            .iter()
+            .map(|t| t.to_string())
+            .collect::<Vec<_>>()
+            .join(", ");
+        format!("({})", r)
+    }
+
+    /// Just checks for length mismatch
+    fn check_arg_params_len(
+        fn_name: &str,
+        arg_len: usize,
+        exp_len: usize,
+    ) -> Result<(), TypeErrors> {
+        if arg_len != exp_len {
+            let e = format!(
+                "Function '{}' takes {} arguments but {} were supplied",
+                fn_name, exp_len, arg_len
+            );
+            return Err(TypeErrors::new_err(&e));
+        }
+
+        Ok(())
+    }
+
+    /// Check if a arg type match given vector of param types. If not, throw a suitable error - report length mismatch or
+    /// type mismatch.
+    pub(crate) fn check_arg_params_match(
+        fn_name: &str,
+        arg_types: &[Type],
+        param_types: &[Type],
+    ) -> Result<(), TypeErrors> {
+        TypeChecker::check_arg_params_len(fn_name, arg_types.len(), param_types.len())?;
+
+        let mut mismatch = false;
+        for (arg, param) in arg_types.iter().zip(param_types.iter()) {
+            if *arg != *param {
+                mismatch = true;
+                break;
+            }
+        }
+
+        if mismatch {
+            let error_msg = format!(
+                "Mismatched types in function call: got ({}) but expected ({})",
+                TypeChecker::get_type_string(arg_types),
+                TypeChecker::get_type_string(param_types),
+            );
+            return Err(TypeErrors::new_err(&error_msg));
+        }
+
+        Ok(())
+    }
+
     // Passed in CheckResult is the accumulated CheckResult from arg checks with ty = Type::Unit
     // arg can be a block, block can have break/return
     pub(crate) fn check_builtin_fn_call(
         &mut self,
         name: &str,
         arg_types: Vec<Type>,
-        check_res: CheckResult,
+        mut check_res: CheckResult,
     ) -> Result<CheckResult, TypeErrors> {
         dbg!("BUILTIN_CHK:", &arg_types);
-        match name {
+
+        check_res.ty = match name {
+            // () -> string
             READ_LINE => {
-                // Fill out this block
+                TypeChecker::check_arg_params_match(name, &arg_types, &[])?;
+                Type::String
             }
+            // (any) -> ()
             PRINT => {
-                // Fill out this block
+                TypeChecker::check_arg_params_len(name, arg_types.len(), 1)?;
+                Type::Unit
             }
+            // (any) -> ()
             PRINTLN => {
-                // Fill out this block
+                TypeChecker::check_arg_params_len(name, arg_types.len(), 1)?;
+                Type::Unit
             }
+            // (string) => int
             STRING_LEN => {
-                // Fill out this block
+                TypeChecker::check_arg_params_match(name, &arg_types, &[Type::String])?;
+                Type::Int
             }
+            // (int, int) => int or (float, float) => float
             MIN => {
-                // Fill out this block
+                TypeChecker::check_arg_params_len(name, arg_types.len(), 2)?;
+                match (arg_types.first().unwrap(), arg_types.get(1).unwrap()) {
+                    (Type::Int, Type::Int) => Type::Int,
+                    (Type::Float, Type::Float) => Type::Float,
+                    _ => {
+                        let e = format!(
+                            "Expected (int, int) or (float, float) but got {}",
+                            TypeChecker::get_type_string(&arg_types)
+                        );
+                        return Err(TypeErrors::new_err(&e));
+                    }
+                }
             }
+            // Same as min
             MAX => {
-                // Fill out this block
+                TypeChecker::check_arg_params_len(name, arg_types.len(), 2)?;
+                match (arg_types.first().unwrap(), arg_types.get(1).unwrap()) {
+                    (Type::Int, Type::Int) => Type::Int,
+                    (Type::Float, Type::Float) => Type::Float,
+                    _ => {
+                        let e = format!(
+                            "Expected (int, int) or (float, float) but got {}",
+                            TypeChecker::get_type_string(&arg_types)
+                        );
+                        return Err(TypeErrors::new_err(&e));
+                    }
+                }
             }
+            // int or float => same type
             ABS => {
-                // Fill out this block
+                TypeChecker::check_arg_params_len(name, arg_types.len(), 1)?;
+                match arg_types.first().unwrap() {
+                    Type::Int => Type::Int,
+                    Type::Float => Type::Float,
+                    _ => {
+                        let e = format!(
+                            "Expected int or float but got {}",
+                            TypeChecker::get_type_string(&arg_types)
+                        );
+                        return Err(TypeErrors::new_err(&e));
+                    }
+                }
             }
+            // float -> float
             COS => {
-                // Fill out this block
+                TypeChecker::check_arg_params_len(name, arg_types.len(), 1)?;
+                match arg_types.first().unwrap() {
+                    Type::Float => Type::Float,
+                    _ => {
+                        let e = format!(
+                            "Expected float but got {}",
+                            TypeChecker::get_type_string(&arg_types)
+                        );
+                        return Err(TypeErrors::new_err(&e));
+                    }
+                }
             }
+            // float -> float
             SIN => {
-                // Fill out this block
+                TypeChecker::check_arg_params_len(name, arg_types.len(), 1)?;
+                match arg_types.first().unwrap() {
+                    Type::Float => Type::Float,
+                    _ => {
+                        let e = format!(
+                            "Expected float but got {}",
+                            TypeChecker::get_type_string(&arg_types)
+                        );
+                        return Err(TypeErrors::new_err(&e));
+                    }
+                }
             }
+            // float -> float
             TAN => {
-                // Fill out this block
+                TypeChecker::check_arg_params_len(name, arg_types.len(), 1)?;
+                match arg_types.first().unwrap() {
+                    Type::Float => Type::Float,
+                    _ => {
+                        let e = format!(
+                            "Expected float but got {}",
+                            TypeChecker::get_type_string(&arg_types)
+                        );
+                        return Err(TypeErrors::new_err(&e));
+                    }
+                }
             }
+            // float -> float
             SQRT => {
-                // Fill out this block
+                TypeChecker::check_arg_params_len(name, arg_types.len(), 1)?;
+                match arg_types.first().unwrap() {
+                    Type::Float => Type::Float,
+                    _ => {
+                        let e = format!(
+                            "Expected float but got {}",
+                            TypeChecker::get_type_string(&arg_types)
+                        );
+                        return Err(TypeErrors::new_err(&e));
+                    }
+                }
             }
+            // float -> float
             LOG => {
-                // Fill out this block
+                TypeChecker::check_arg_params_len(name, arg_types.len(), 1)?;
+                match arg_types.first().unwrap() {
+                    Type::Float => Type::Float,
+                    _ => {
+                        let e = format!(
+                            "Expected float but got {}",
+                            TypeChecker::get_type_string(&arg_types)
+                        );
+                        return Err(TypeErrors::new_err(&e));
+                    }
+                }
             }
+            // float, float => float
             POW => {
-                // Fill out this block
+                TypeChecker::check_arg_params_len(name, arg_types.len(), 2)?;
+                match (arg_types.first().unwrap(), arg_types.get(1).unwrap()) {
+                    (Type::Float, Type::Float) => Type::Float,
+                    _ => {
+                        let e = format!(
+                            "Expected (float, float) but got {}",
+                            TypeChecker::get_type_string(&arg_types)
+                        );
+                        return Err(TypeErrors::new_err(&e));
+                    }
+                }
             }
+            // int -> string
             ITOA => {
-                // Fill out this block
+                TypeChecker::check_arg_params_len(name, arg_types.len(), 1)?;
+                match arg_types.first().unwrap() {
+                    Type::Int => Type::String,
+                    _ => {
+                        let e = format!(
+                            "Expected int but got {}",
+                            TypeChecker::get_type_string(&arg_types)
+                        );
+                        return Err(TypeErrors::new_err(&e));
+                    }
+                }
             }
+            // string -> int
             ATOI => {
-                // Fill out this block
+                TypeChecker::check_arg_params_len(name, arg_types.len(), 1)?;
+                match arg_types.first().unwrap() {
+                    Type::String => Type::Int,
+                    _ => {
+                        let e = format!(
+                            "Expected string but got {}",
+                            TypeChecker::get_type_string(&arg_types)
+                        );
+                        return Err(TypeErrors::new_err(&e));
+                    }
+                }
             }
+            // float -> int
             FLOAT_TO_INT => {
-                // Fill out this block
+                TypeChecker::check_arg_params_len(name, arg_types.len(), 1)?;
+                match arg_types.first().unwrap() {
+                    Type::Float => Type::Int,
+                    _ => {
+                        let e = format!(
+                            "Expected float but got {}",
+                            TypeChecker::get_type_string(&arg_types)
+                        );
+                        return Err(TypeErrors::new_err(&e));
+                    }
+                }
             }
+            // int -> float
             INT_TO_FLOAT => {
-                // Fill out this block
+                TypeChecker::check_arg_params_len(name, arg_types.len(), 1)?;
+                match arg_types.first().unwrap() {
+                    Type::Int => Type::Float,
+                    _ => {
+                        let e = format!(
+                            "Expected int but got {}",
+                            TypeChecker::get_type_string(&arg_types)
+                        );
+                        return Err(TypeErrors::new_err(&e));
+                    }
+                }
             }
             SEM_CREATE => {
                 // Fill out this block
+                todo!()
             }
             SEM_SET => {
                 // Fill out this block
+                todo!()
             }
-            _ => (),
+            _ => todo!(),
         };
 
         Ok(check_res)
@@ -175,12 +385,59 @@ mod tests {
 
     use crate::type_checker::expect_pass;
 
-    use super::{BUILTINS, READ_LINE};
+    use super::BUILTINS;
 
     #[test]
-    fn test_builtin_sym() {
+    fn test_type_check_builtin_sym() {
         for &builtin in BUILTINS.iter() {
             expect_pass(builtin, Type::BuiltInFn);
         }
+    }
+
+    #[test]
+    fn test_type_check_builtin_functions() {
+        expect_pass("let x : () = print(2); x", Type::Unit);
+
+        // Test min
+        expect_pass("let x : int = min(2, 3); x", Type::Int);
+        expect_pass("let x : float = min(2.0, 3.0); x", Type::Float);
+
+        // Test max
+        expect_pass("let x : int = max(2, 3); x", Type::Int);
+        expect_pass("let x : float = max(2.0, 3.0); x", Type::Float);
+
+        // Test abs
+        expect_pass("let x : int = abs(-5); x", Type::Int);
+        expect_pass("let x : float = abs(-5.0); x", Type::Float);
+
+        // Test cos
+        expect_pass("let x : float = cos(0.0); x", Type::Float);
+
+        // Test sin
+        expect_pass("let x : float = sin(0.0); x", Type::Float);
+
+        // Test tan
+        expect_pass("let x : float = tan(0.0); x", Type::Float);
+
+        // Test sqrt
+        expect_pass("let x : float = sqrt(4.0); x", Type::Float);
+
+        // Test log
+        expect_pass("let x : float = log(1.0); x", Type::Float);
+
+        // Test pow
+        expect_pass("let x : float = pow(2.0, 3.0); x", Type::Float);
+
+        // Test itoa
+        // expect_pass("let x : string = itoa(123); x", Type::String);
+
+        // Test atoi
+        // expect_pass("let x : int = atoi(\"123\"); x", Type::Int);
+
+        // Test float_to_int
+        expect_pass("let x : int = float_to_int(3.5); x", Type::Int);
+
+        // Test int_to_float
+        expect_pass("let x : float = int_to_float(3); x", Type::Float);
     }
 }

1d3d0716d39449d1c0da71818cfcaee7ed2efb5c
Huzaifa Raghav <huzaifaraghav2000@gmail.com>
2024-04-18 21:13:01 +0800

    Add hof e2e

--- vm/ignite/tests/e2e.rs
+++ vm/ignite/tests/e2e.rs
@@ -565,5 +565,23 @@ fn test_e2e_fn_decl() -> Result<()> {
     ";
     test_pass(t, "4")?;
 
+    let hof = r"
+    fn fac_tail(n: int) -> int {
+        fn tail(n: int, acc: int) -> int {
+            if n == 0 {
+                return acc;
+            } else {
+                return tail(n-1, acc * n);
+            }   
+        }
+    
+        tail(n, 1)
+    }
+    
+    let res : int = fac_tail(4);
+    res // 24
+    ";
+    test_pass(hof, "24")?;
+
     Ok(())
 }
